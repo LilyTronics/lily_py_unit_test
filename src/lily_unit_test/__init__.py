36c125675767d636dcace7bc5643ea997f5e29e6
Danny van der Pol <danny.vanderpol@teledyne.com>
2024-01-05 09:45:17 +0100

    add object with classification values

--- src/lily_unit_test/__init__.py
+++ src/lily_unit_test/__init__.py
@@ -2,10 +2,11 @@
 Lily unit test package
 """
 
+from lily_unit_test.models.classification import Classification
 from lily_unit_test.models.test_runner import TestRunner
 from lily_unit_test.models.test_suite import TestSuite
 
-
 # For easy import:
+Classification = Classification
 TestRunner = TestRunner
 TestSuite = TestSuite
