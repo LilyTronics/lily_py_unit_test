09e443ca60885baf271f95c84fcf84d64a03daa9
Danny van der Pol <danny.vanderpol@teledyne.com>
2024-01-29 08:59:22 +0100

    add doc string for the autodoc

--- src/lily_unit_test/models/test_suite.py
+++ src/lily_unit_test/models/test_suite.py
@@ -9,6 +9,15 @@ from lily_unit_test.models.logger import Logger
 
 
 class TestSuite(object):
+    """
+    Base class for all test suites.
+
+    :param report_path: path were the reports are stored.
+
+    The test runner creates the report path and passes it to the test suite. This path can be used in the tests.
+    Setting this path here will not change the path where the reports are stored.
+    This is determined by the test runner (see test runner class).
+    """
 
     CLASSIFICATION = Classification.PASS
 
@@ -17,6 +26,15 @@ class TestSuite(object):
         self.log = Logger()
 
     def run(self, log_traceback=False):
+        """
+        Run the test suite.
+
+        :param log_traceback: if True, detailed traceback information is written to the logger in case of an exception.
+        :return: True when all tests are passed, False when one or more tests are failed.
+
+        The run method creates a list of all methods starting with :code:`test_`. Before executing the test methods,
+        it executes the setup method. After executing the test methods, it executes the teardown.
+        """
         test_suite_name = self.__class__.__name__
         self.log.info("Run test suite: {}".format(test_suite_name))
 
@@ -100,26 +118,65 @@ class TestSuite(object):
         return test_suite_result
 
     def get_report_path(self):
+        """
+        Get the path to the report files as set by the test runner.
+
+        :return: string containing the path to the report files.
+        """
         return self._report_path
 
     ##############################
     # Override these when needed #
     ##############################
 
-    def setup(self): return True
-    def teardown(self): pass
+    def setup(self):
+        """
+        The setup method. This can be overridden in the test suite. This will be executed before running all test
+        methods.
+
+        :return: True or None when the setup is passed, False when the setup is failed.
+
+        The test methods are executed after the setup is executed successfully.
+        If the setup fails because of either an exception or returning False, the test methods are not executed.
+        """
+        return True
+
+    def teardown(self):
+        """
+        The teardown method. This can be overridden in the test suite. This will be executed after running all test
+        methods.
+
+        This method is always executed and if there is an exception raise in this method, the test suite is reported
+        as failed.
+        """
+        pass
 
     ################
     # Test methods #
     ################
 
     def fail(self, error_message, raise_exception=True):
+        """
+        Make the test suite fail.
+
+        :param error_message: the error message that should be written to the logger.
+        :param raise_exception: if True, an exception is raised and the test suit will stop.
+        :return: False
+        """
         self.log.error(error_message)
         if raise_exception:
             raise Exception(error_message)
         return False
 
     def fail_if(self, expression, error_message, raise_exception=True):
+        """
+        Fail if the given expression evaluates to True.
+
+        :param expression: the expression that should be evaluated.
+        :param error_message: the error message that should be written to the logger.
+        :param raise_exception: if True, an exception is raised and the test suit will stop.
+        :return: False in case of a failure
+        """
         if expression:
             self.fail(error_message, raise_exception)
 
