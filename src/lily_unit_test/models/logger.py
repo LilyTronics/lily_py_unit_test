96428b9158dff5ff10bfc6da0d90654f46a7fb45
Danny van der Pol <danny.vanderpol@teledyne.com>
2024-01-29 10:58:48 +0100

    add docstrings for autodoc

--- src/lily_unit_test/models/logger.py
+++ src/lily_unit_test/models/logger.py
@@ -10,6 +10,38 @@ from datetime import datetime
 
 
 class Logger(object):
+    """
+    Logger class.
+    Handles all log messages and messages from stdout and stderr.
+    The logger is part of the test suite and can be accessed by: :code:`TestSuite.log`.
+
+    :param redirect_std: if True, stdout and stderr are redirected to the logger.
+    :param log_to_stdout: if True, log messages are written to the stdout (console).
+
+    | All log messages are stored to an internal buffer (list of strings).
+    | All log messages have the following format:
+
+    :code:`"<timestamp> | <type> | <message>"`
+
+    | :code:`<timestamp>`: date and time of the message. Time is up to 1ms accurate.
+    | :code:`<type>`: Type of the message.
+    | :code:`<message>`: The message itself.
+
+    The message type can have one of the following values:
+
+    ======== =================================================================================
+    Type     Description
+    ======== =================================================================================
+    | INFO   | Informational message, usually for indicating generic test messages.
+    | DEBUG  | Debug message, usually for logging content of variables or more detailed test
+             | messages.
+    | ERROR  | Error message, for reporting an error.
+    | STDOUT | Standard output messages, messages that are written to standard output handler,
+             | usually when using :code:`print()`.
+    | STDERR | Standard error messages, messages that are written to standard error handler,
+             | usually when an exception is raised.
+    ======== =================================================================================
+    """
 
     TYPE_INFO = "INFO"
     TYPE_DEBUG = "DEBUG"
@@ -45,25 +77,63 @@ class Logger(object):
             sys.stderr = self._StdLogger(self, self.TYPE_STDERR)
 
     def get_log_messages(self):
+        """
+        Returns a reference to the log messages buffer.
+
+        :return: reference to the list of strings containing all log messages.
+
+        | Note that it returns a reference, meaning any changes the logger makes to the list will affect the reference.
+        | To get a static copy of the log messages use: :code:`get_log_messages().copy()`.
+        | This will return a new list with a copy of all the log messages at that moment.
+        """
         return self._log_messages
 
     def shutdown(self):
+        """
+        Shutdown the logger.
+        This will restore the original stdout and stderr handlers.
+        """
         sys.stdout = self._orgStdout
         sys.stderr = self._orgStderr
 
     def info(self, message):
+        """
+        Log a 'info' type message.
+
+        :param message: the message to write to the logger.
+        """
         self.handle_message(self.TYPE_INFO, "{}\n".format(message))
 
     def debug(self, message):
+        """
+        Log a 'debug' type message.
+
+        :param message: the message to write to the logger.
+        """
         self.handle_message(self.TYPE_DEBUG, "{}\n".format(message))
 
     def error(self, message):
+        """
+        Log a 'error' type message.
+
+        :param message: the message to write to the logger.
+        """
         self.handle_message(self.TYPE_ERROR, "{}\n".format(message))
 
     def empty_line(self):
+        """
+        Adds an empty line in the log messages.
+        """
         self.handle_message(self.TYPE_EMPTY_LINE, "")
 
     def handle_message(self, message_type, message_text):
+        """
+        Handles the message of a given type. This method is use by :code:`info()`, :code:`debug()`, :code:`error()` and
+        :code:`empty_line()`. It is not encouraged to use this function, use with caution.
+
+        :param message_type: a string indicating the message type (see table above).
+        :param message_text: the message to write to the logger.
+        """
         if message_type == self.TYPE_EMPTY_LINE:
             self._log_messages.append("")
             if self._log_to_stdout:
