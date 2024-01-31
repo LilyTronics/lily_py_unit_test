1741c8832e68d3155fcfee62afb48de31d39c496
Danny van der Pol <danny.vanderpol@teledyne.com>
2024-01-31 09:51:52 +0100

    fix paths

--- src/run_tests.py
+++ src/run_tests.py
@@ -8,16 +8,23 @@ from lily_unit_test import TestRunner
 from lily_unit_test.test_settings import TestSettings
 
 
-report_folder = os.path.abspath(os.path.join(os.path.dirname("../"), TestSettings.REPORT_FOLDER_NAME))
-if not os.path.isdir(report_folder):
-    os.makedirs(report_folder)
+def run_unit_tests():
+    root_path = os.path.dirname(__file__)
+    report_folder = os.path.join(os.path.dirname(root_path), TestSettings.REPORT_FOLDER_NAME)
+    if not os.path.isdir(report_folder):
+        os.makedirs(report_folder)
 
-options = {
-    "report_folder": report_folder,
-    "create_html_report": True,
-    "open_in_browser": True,
-    "run_first": "TestEnvironmentSetup",
-    "run_last": "TestEnvironmentCleanup"
-}
+    options = {
+        "report_folder": report_folder,
+        "create_html_report": True,
+        "open_in_browser": True,
+        "run_first": "TestEnvironmentSetup",
+        "run_last": "TestEnvironmentCleanup"
+    }
 
-TestRunner.run("./test_suites", options)
+    TestRunner.run(os.path.join(root_path, "test_suites"), options)
+
+
+if __name__ == "__main__":
+
+    run_unit_tests()
