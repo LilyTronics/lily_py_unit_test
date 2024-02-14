94e06b84c4c89baf6966dcabcd4e91b00171c484
Danny <danny@lilytronics.nl>
2024-02-14 10:46:03 +0100

    disable the self-assigning-variable for this file

--- src/lily_unit_test/__init__.py
+++ src/lily_unit_test/__init__.py
@@ -2,12 +2,14 @@
 Lily unit test package
 """
 
+# Disable message for self-assigning-variable in this file
+# pylint: disable-msg=W0127
+
 from lily_unit_test.classification import Classification
 from lily_unit_test.logger import Logger
 from lily_unit_test.test_settings import TestSettings
 from lily_unit_test.test_runner import TestRunner
 from lily_unit_test.test_suite import TestSuite
-
 
 # For easy import:
 Classification = Classification
